d880b8014b92b3287bd9f7094fa42e305a11db8c
Juliette <juliette.gregoire@etu.umontpellier.fr>
2018-11-09 14:44:34 +0100

    Frimirno 1st commit - index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="icon" href="img/favicon.ico" />
+		<title>Supernatural.com - Home</title>
+	</head>
+	<body>
+		<header>
+			
+		</header>
+		<main>
+			
+		</main>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
