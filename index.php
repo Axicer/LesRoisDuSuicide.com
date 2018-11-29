90378dde6b974ce60c241053bddf51202e5e1425
Axicer <julienrabez@gmail.com>
2018-11-29 23:21:24 +0100

    http://prntscr.com/lopy8r

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="icon" href="res/imgs/favicon.ico" />
-	<title><?php echo $title ?></title>
-	<link rel="stylesheet" type="text/css" href="styles/main.css">
-</head>
-
 <?php
 	require_once "lib/File.php";
 	require File::build_path(array("controller", "router.php"));
 ?>
-</html>
