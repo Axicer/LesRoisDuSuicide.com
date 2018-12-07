b1ca6b09b56713eada8c980366a0a610c3db7820
Juliette <juliette.gregoire@etu.umontpellier.fr>
2018-12-07 13:23:01 +0100

    page produit validated html5

--- view/produits/list.php
+++ view/produits/list.php
@@ -4,21 +4,19 @@
 
 	<?php
 	foreach ($products as $p) {
-		?>
-		<div class="un_produit">
-			<div class="info_produit">
-				<div> <?php echo $p->nom; ?></div>
-				<div> <?php echo $p->prix; ?>€</div>
-				<div> <?php echo $p->description; ?></div>
-				<img class="image_P" src="res/imgs/<?php echo $p->imageProduit ?>">
+		echo "<div class=\"un_produit\">
+			<div class=\"info_produit\">
+				<div>".$p->nom."</div>
+				<div>".$p->prix."€</div>
+				<div>".$p->description."</div>
+				<img class=\"image_P\" src=\"res/imgs/".$p->imageProduit."\" alt=\"".$p->nom."\">
 			</div>
-			<div class="param">
-				<a href="./?page=produits&action=specific&id=<?php echo $id; ?>">En savoir plus !</a>
-				<a href="./?page=panier&action=add&id=<?php echo $id; ?>">Ajouter au panier !</a>
+			<div class=\"param\">
+				<a href=\"./?page=produits&action=specific&id=".$p->idProduit."\">En savoir plus !</a>
+				<a href=\"./?page=panier&action=add&id=".$p->idProduit."\">Ajouter au panier !</a>
 			</div>
 		</div>
-		<hr>
-		<?php
+		<hr>";
 	}
 	?>
 </div>
