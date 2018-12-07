9183eccc2e775a2a05c276436f5800d43571e9de
Juliette <juliette.gregoire@etu.umontpellier.fr>
2018-12-07 15:00:18 +0100

    css of connexion page done

--- view/login/login_form.php
+++ view/login/login_form.php
@@ -1,17 +1,17 @@
-<link rel="stylesheet" type="text/css" href="styles/connexion/main.css">
-<p>Connectez vous a votre site favori de science occulte !</p>
+<link rel="stylesheet" type="text/css" href="styles/connexion/login.css">
+<p class="petit_mot_gentil">Connectez vous a votre site favori de science occulte !</p>
 <form action="./?page=login" method="post">
-	<fieldset>
+	<fieldset class="log_fieldset">
 		<div id="login_field">
-		    <label for="login">Login</label> :
+		    <label for="login">Login </label>
 		    <input type="text" placeholder="login" name="login" id="login_field" required/>
 		</div>
 		<div id="mdp">
-		    <label for="mdp">Mot de passe</label> :
+		    <label for="mdp">Mot de passe </label>
 		    <input type="password" placeholder="Mot de passe" name="mdp" id="mdp" required/>
 		</div>
 
 		<input type="hidden" name="action" value="logged"/>
-		<input type="submit" value="Envoyer"/>
+		<input type="submit" value="Connexion"/>
 	</fieldset>
 </form>
