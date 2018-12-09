52527cc296a12eb0476122cd06a3b6418a556b5a
Axicer <julienrabez@gmail.com>
2018-12-09 14:28:05 +0100

    little fix

--- view/produits/produits_form.php
+++ view/produits/produits_form.php
@@ -1,5 +1,5 @@
 <p>Trouver votre produit !</p>
-<form action="./page=produits" method="POST">
+<form action="./?page=produits" method="POST">
 
 	<label for="name">Recherche</label>:
 	<input type="text" placeholder="Entrer un terme de recherche" name="name" id="name" required/>
