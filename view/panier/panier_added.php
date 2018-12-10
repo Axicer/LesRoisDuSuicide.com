c8d4438805b5bdf9342606a7a3b1602689a56d3a
pechh <pechh@K133-23.dep-info.iutmontp.univ-montp2.fr>
2018-12-10 16:55:56 +0100

    Ajout d'un message mieux pour l'ajout au panier

--- view/panier/panier_added.php
+++ view/panier/panier_added.php
@@ -1,2 +1,6 @@
-<p>Le produit a été ajouté a votre panier avec succés !</p>
-<a href="./?page=accueil">Retour a l'accueil</a>
+<p class="un_produit"><b>Le produit a été ajouté a votre panier avec succès !</b></p>
+
+<?php
+$products = Query::getAllProducts(50);
+require(File::build_path(array('view', 'produits', 'produits_list.php')))
+?>
